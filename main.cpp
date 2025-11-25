fffc4966c84b03020f01044ffabf79ae016dbba5
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2025-11-25 22:15:38 +0500

    implement book add and display using linked list

---
+++ main.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Node {
+public:
+    int id;
+    string title;
+    string author;
+    float price;
+    bool isIssued;
+    Node* next;
+
+    Node(int id, string title, string author, float price) {
+        this->id = id;
+        this->title = title;
+        this->author = author;
+        this->price = price;
+        this->isIssued = false;
+        next = nullptr;
+    }
+};
+
+struct Book {
+    int ID;
+    string title;
+    string author;
+    float price;
+};
+
+class LinkedList {
+public:
+    Node* head = nullptr;
+
+    void push(int new_val, string new_title, string new_author, int new_price) {
+        Node* new_node = new Node(new_val, new_title, new_author, new_price);
+        if (head == nullptr) {
+            head = new_node;
+            return;
+        }
+        Node* curr = head;
+        while (curr->next != nullptr) {
+            curr = curr->next;
+        }
+        curr->next = new_node;
+        cout << "Book added successfully!" << endl;
+        cout << endl;
+    }
+
+    void print_nodes() {
+        Node* curr = head;
+        while (curr != nullptr) {
+            cout << "================================================================" << endl;
+            cout << "Book ID: " << curr->id << endl;
+            cout << "Book Title: " << curr->title << endl;
+            cout << "Book Author: " << curr->author << endl;
+            cout << "Book Price: " << curr->price << endl;
+            cout << "Is Issued: " << curr->isIssued << endl;
+            cout << "================================================================" << endl;
+            cout << endl;
+            curr = curr->next;
+        }
+    }
+
+    void search_book()
+    {
+        // enter code here with binary search and sorting
+    }
+
+    void issue_book()
+    {
+        // enter code here for issuing the book
+    }
+
+    void return_book()
+    {
+        // enter code here for return book
+    }
+
+    void available_books()
+    {
+        // enter code here for availible books
+    }
+
+    void issued_books()
+    {
+        // enter code here for issued book
+    }
+};
+
+LinkedList* l;
+
+void Add_book() {
+    cout << "Enter no. of books to be added: " << endl;
+    int n;
+    cin >> n;
+    while (n != 0) {
+        Book b;
+        cout << endl;
+        cout << "Enter book ID: ";
+        cin >> b.ID;
+        cin.ignore();
+        cout << "Enter book Title: ";
+        getline(cin, b.title);
+        cout << "Enter book Author name: ";
+        getline(cin, b.author);
+        cout << "Enter book price: ";
+        cin >> b.price;
+        cin.ignore();
+        l->push(b.ID, b.title, b.author, b.price);
+        n--;
+    };
+}
+
+void print_books() {
+    l->print_nodes();
+}
+
+int main()
+{
+    l = new LinkedList();
+    int choice;
+    do {
+        cout << "1. Add Book" << endl;
+        cout << "2. Show all Books" << endl;
+        cout << "3. Search Book" << endl;
+        cout << "4. Issue Book" << endl;
+        cout << "5. Return Book" << endl;
+        cout << "6. Show Available Books" << endl;
+        cout << "7. Show Issued Books" << endl;
+        cout << "0. Exit" << endl;
+        cout << "Enter choice: ";
+        cin >> choice;
+        if (choice == 1) {
+            Add_book();
+        }
+        else if (choice == 2) {
+            print_books();
+        }
+        else if (choice == 3) {
+            cout << "Working on it" << endl;
+        }
+        else {
+            break;
+        }
+    } while (choice != 0);
+    return 0;
+}
